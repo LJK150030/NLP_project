c7f3254143852e0883e1489fc8676812097be9b9
Lawrence Klinkert <JKlinkert@SMU.edu>
2022-05-04 10:07:52 -0500

    Looking at autobiographies from Benjamin Franklin in the standardised project gutenberg corpus, and checking comparision. Updated projected word calculation, and added dot product calculation, and finaly comparing document matrix with paragraph matrix.

--- maneframe.py
+++ maneframe.py
@@ -1,9 +1,15 @@
+import scipy.sparse
 import numpy as np
 import os 
-#import enchant
-from nltk.corpus import words
+import enchant
 from sklearn.feature_extraction import text
+from sklearn.preprocessing import normalize
+from sklearn import metrics
 from nltk.corpus import stopwords
+from scipy.spatial import distance
+from scipy.special import softmax
+from scipy import sparse
+from sklearn.preprocessing import normalize
 from datetime import datetime
 
 # HELPPER FUNCTIONS
@@ -30,14 +36,23 @@ def Distance(a, b):
     sum_square = np.dot(diff, diff.T)
     #print(f"sum_square: {sum_square.shape}")
     return np.sqrt(sum_square)
-
+    
+def GeneralNorm(x, min_in, max_in, min_out, max_out):
+    denominator = max_in - min_in
+    if denominator == 0.0:
+        return 0.0
+    
+    numerator = (x - min_in) * (max_out - min_out)
+    frac = numerator/denominator
+    result = frac + min_out
+    #print(f"{result} = {numerator}/{denominator} + {min_out}")
+    return result
+    
 
 # Global constant variables
 CURRENT_DIRECTORY = os.getcwd()
-DIR_OF_TEXT = os.path.join(CURRENT_DIRECTORY, "en_data/text")
-FILE_PATH_TEXTS = GetListOfFileNames(DIR_OF_TEXT)
 STOP_WORDS = stopwords.words('english')
-#EN_DICT = enchant.Dict("en_US")
+EN_DICT = enchant.Dict("en_US")
 
 POLAR_WORD_SETS =[
     ["negative", "positive"],
@@ -50,41 +65,41 @@ POLAR_WORD_SETS =[
     ["reproach", "admiration"],
     ["hate", "love"],
     ["disgust", "interest"],
+    ['remorse', 'gratification'],
+    ['anger', 'gratitude'],
     ["fears-confirmed", "satisfaction"],
     ["disappointment", "relief"],
     ["resentment", "happy-for"],
     ["pity", "gloating"]
 ]
 
-polar_word_index =[
-    [-1, -1],
-    [-1, -1],
-    [-1, -1],
-    [-1, -1],
-    [-1, -1],
-    [-1, -1],
-    [-1, -1],
-    [-1, -1],
-    [-1, -1],
-    [-1, -1],
-    [-1, -1],
-    [-1, -1],
-    [-1, -1],
-    [-1, -1]
-]
-
+polar_word_index = np.full((16, 2), -1, dtype=np.int_)
 
 # Init document-token matrix
 embedding_sets = {
     "count_vectorizer": text.CountVectorizer,
-    "tfidf_vectorizer": text.TfidfVectorizer,
-    "hashing_vectorizer": text.HashingVectorizer
+    "tfidf_vectorizer": text.TfidfVectorizer
+    #"hashing_vectorizer": text.HashingVectorizer
 }
 
+CALCULATION = [
+    "projection",
+    "dot_product"
+]
 
+
+# ---------------------------------------------------------------------------------------
+# Change these variables for the specific procedures, and where to find the documents
 CURRENT_SET = "count_vectorizer"
+CURRENT_CALC = "projection"
+DIR_OF_TEXT = os.path.join(CURRENT_DIRECTORY, "en_data/benjamin_franklin_paragraphs")
+FILE_PATH_TEXTS = GetListOfFileNames(DIR_OF_TEXT)
+# ---------------------------------------------------------------------------------------
 
 
+# Hour long computation, only do if necessary...
+
+print(f"Finding all valid tokens in Documents...")
 vectorizer_spgc = embedding_sets[CURRENT_SET](  input="filename", 
                                     encoding='utf-8',
                                     decode_error='ignore',
@@ -93,20 +108,51 @@ vectorizer_spgc = embedding_sets[CURRENT_SET](  input="filename",
                                     stop_words=STOP_WORDS,
                                     ngram_range = (1, 1),
                                     analyzer='word',
-                                    dtype=np.float64)
+                                    dtype=np.float32)
+
+# Construct document-token matrix, then transpose for token-document matrix
+count_embeding = vectorizer_spgc.fit_transform(FILE_PATH_TEXTS)
+
+print(f"term_freq_document shape: {count_embeding.transpose().shape}")
+feature_list = vectorizer_spgc.get_feature_names_out()
+
+print(f"Finding valid words from tokenizer...")
+sub_word_list = list()
+for token in feature_list:
+    if any(char.isdigit() for char in token):
+        continue
+    if EN_DICT.check(token):
+        sub_word_list.append(token)
+
+fraction_of_words = len(sub_word_list)/len(feature_list)
+
+print(f"From the {len(feature_list)} tokens, only {len(sub_word_list)} are words. A {(1.0 - fraction_of_words) * 100.0}% reduction")
+
+vocab_dict = {word.lower(): idx for idx, word in enumerate(sub_word_list)}
+
+print(f"Using words as vocabulary for embedding step...")
+vectorizer_spgc = embedding_sets[CURRENT_SET](  input="filename", 
+                                                encoding='utf-8',
+                                                decode_error='ignore',
+                                                strip_accents='unicode',
+                                                lowercase=True,
+                                                stop_words=STOP_WORDS,
+                                                ngram_range = (1, 1),
+                                                analyzer='word',
+                                                vocabulary= vocab_dict,
+                                                dtype=np.float32)
 
 # Construct document-token matrix, then transpose for token-document matrix
 count_embeding = vectorizer_spgc.fit_transform(FILE_PATH_TEXTS)
 count_embeding = count_embeding.transpose()
 
-# At this point, do some clean up so we can make some room to calculate tocken positions
+print(f"term_freq_document shape: {count_embeding.shape}")
+feature_list = vectorizer_spgc.get_feature_names_out()
+
 FILE_PATH_TEXTS.clear()
 
 
-print(f"term_freq_document shape: {count_embeding.shape}")
-
 # From the vocabulary created by CountVectorizer, find the token index. If not found, keep -1
-feature_list = vectorizer_spgc.get_feature_names_out()
 for word_set_idx in range(len(POLAR_WORD_SETS)):
     for word_idx in range(len(POLAR_WORD_SETS[word_set_idx])):
         for (index, item) in enumerate(feature_list):
@@ -117,8 +163,11 @@ for word_set_idx in range(len(POLAR_WORD_SETS)):
 print(polar_word_index)
 
 
-# TODO: Need to run this through all polar tokens
-start=datetime.now()
+polar_set_midpoints = np.zeros(shape=(len(POLAR_WORD_SETS), count_embeding.shape[1]), dtype=np.float32)
+semaxis_norms = np.zeros(shape=(len(POLAR_WORD_SETS), count_embeding.shape[1]), dtype=np.float32)
+polar_set_strings = list()
+
+
 for word_set_idx in range(len(POLAR_WORD_SETS)):
     negative_idx = polar_word_index[word_set_idx][0] 
     positive_idx = polar_word_index[word_set_idx][1]
@@ -127,69 +176,132 @@ for word_set_idx in range(len(POLAR_WORD_SETS)):
     
     if negative_idx == -1 or positive_idx == -1:
         continue 
-
-    print(f"Calculating SemAxis: {negative_str}_{positive_str}")
     
     negative_point = count_embeding[negative_idx].toarray()
     positive_point = count_embeding[positive_idx].toarray()
-    #print(negative_point.shape)
-    #print(positive_point.shape)
-
     pos_neg_vector = positive_point - negative_point
     semaxis_norm = pos_neg_vector / np.sqrt(np.sum(pos_neg_vector**2))
     midpoint = pos_neg_vector/2.0
 
-    max_dist = 0.0
-    token_distances = dict()
-    num_words = 0
-    for idex in range(count_embeding.shape[0]):
+    semaxis_norms[word_set_idx] = semaxis_norm
+    polar_set_midpoints[word_set_idx] = midpoint[0]
+    polar_set_strings.append(f"{negative_str}_{positive_str}")
 
-        if idex % 100000 == 0:
-            print(f"Processed: {idex} tokens, found {num_words} words after {datetime.now()-start}")
+polar_set_midpoints = sparse.csr_matrix(polar_set_midpoints, dtype=np.float32)
+semaxis_norms = sparse.csr_matrix(semaxis_norms, dtype=np.float32)
 
-        token_string = feature_list[idex]
-
-        if any(char.isdigit() for char in token_string):
-            continue
-
-        #if not EN_DICT.check(token_string):
-        #    continue
-        if not token_string in words.words():
-            continue
-
-        projected_point = VectorProjection(count_embeding[idex].toarray(), semaxis_norm.T)
-        #print(f"midpoint: {midpoint.shape}")
-        #print(f"projected_point: {projected_point.shape}")
-        dot_product = np.dot(projected_point.T, midpoint.T)
-        #print(f"dot_product: {dot_product.shape}")
-        #print(f"projected_point.T: {projected_point.T.shape}")
-        #print(f"midpoint: {midpoint.shape}")
-        dist = Distance(projected_point.T, midpoint)
-
-        if max_dist < dist:
-            max_dist = dist
-
-        if dot_product < 0.0:
-            dist = -dist
-
-        token_distances[token_string] = dist
-        num_words += 1
-        #print(f"{token_string},{token_distances[token_string]}")
-        
+print(polar_set_midpoints.shape)
+print(semaxis_norms.shape)
 
 
-    for key in token_distances:
-        token_distances[key] = token_distances[key]/max_dist
+print(f"Normalizing count embeding...")
+count_embeding_normalized = normalize(count_embeding, norm='l2', axis=1)
+
+## Calculate projected word position
+print(f"Calculating projected word...")
+print(f"...numerator calculation...")
+numerator = metrics.pairwise.cosine_similarity( X = count_embeding_normalized,
+                                                Y = semaxis_norms)
+numerator = sparse.csr_matrix(numerator)
+scalar = sparse.linalg.norm(x=count_embeding.transpose(), axis=0)
+scalar = np.resize(a=scalar, new_shape=(count_embeding.shape[0], len(POLAR_WORD_SETS)))
+print(f"numerator: {numerator.shape}")
+print(f"scalar: {scalar.shape}")
+
+print(f"...denominator calculation...")
+denominator = sparse.linalg.norm(   x=semaxis_norms,
+                                    axis=1)
+
+#denominator = np.linalg.norm(x=semaxis_norms, axis=1)
+#denominator = np.resize(a=denominator, new_shape=(semaxis_norms.shape[1], len(POLAR_WORD_SETS)))
+
+numerator = numerator.multiply(scalar)
+print(f"denominator: {denominator.shape}")
+
+print(f"...projection scalar calculation...")
+#word_frac = np.divide(numerator, denominator)
+word_frac = numerator/denominator
+word_frac = sparse.csr_matrix(word_frac)
+print(f"word_frac: {word_frac.shape}")
+
+# This block is questionable, sometimes it asks for too much memory, and othertimes it works fine.
+
+print(f"...final word projection calculation...")
+dir_for_set = os.path.join(CURRENT_DIRECTORY, f"en_data/{CURRENT_SET}/")
+if not os.path.isdir(dir_for_set):
+    os.mkdir(dir_for_set)
+dir_for_set = os.path.join(dir_for_set, f"{CURRENT_CALC}")
+if not os.path.isdir(dir_for_set):
+    os.mkdir(dir_for_set)
+
+for polar_set_idx in range(len(POLAR_WORD_SETS)):
+    negative_idx = polar_word_index[polar_set_idx][0] 
+    positive_idx = polar_word_index[polar_set_idx][1]
+    negative_str = POLAR_WORD_SETS[polar_set_idx][0]
+    positive_str = POLAR_WORD_SETS[polar_set_idx][1]
 
 
-    dir_for_set = os.path.join(CURRENT_DIRECTORY, f"en_data/{CURRENT_SET}/")
+    if negative_idx == -1 or positive_idx == -1:
+        continue
 
-    if not os.path.isdir(dir_for_set):
-        os.mkdir(dir_for_set) 
+    print(f"...Calculating polar set {polar_set_idx}_{negative_str}_{positive_str}...")
+    
+    #print(f"semaxis_norms: {semaxis_norms[polar_set_idx].shape}")
+    #print(f"word_frac: {word_frac.T[polar_set_idx].shape}")
+    word_projected_point = semaxis_norms[polar_set_idx].T @ word_frac.T[polar_set_idx]
+    word_projected_point = sparse.csr_matrix(word_projected_point.T)
+    #print(f"word_projected_point: {word_projected_point.shape}")
 
-    dir_for_polar = os.path.join(dir_for_set, f"{negative_str}_{positive_str}.csv")
+    # Distance of the word from the midpoint
+    word_distance = metrics.pairwise.pairwise_distances(X=word_projected_point,
+                                                        Y=midpoint,
+                                                        metric = "euclidean",
+                                                        n_jobs=-1,
+                                                        force_all_finite = True)
 
-    with open(dir_for_polar, 'w') as f:
-        f.write(f"token, distance\n")
-        for key in token_distances.keys():
-            f.write(f"{key},{token_distances[key]}\n")
+    #print(f"count_embeding_normalized: {count_embeding_normalized[1]}")
+    #print(f"word_projected_point: {word_projected_point[0]}")
+    #print(f"word_distance: {word_distance}")
+    #max = np.amax(word_distance) 
+    #word_distance = word_distance/max
+    word_distance = sparse.csr_matrix(word_distance)
+    min_dist = sparse.csr_matrix.min(word_distance)
+    max_dist = sparse.csr_matrix.max(word_distance)
+    # Used to determine polarity
+    change_basis_word_pos = count_embeding - midpoint
+    word_dot_product = metrics.pairwise.linear_kernel(  X = change_basis_word_pos,
+                                                        Y = semaxis_norms)
+    word_dot_product = sparse.csr_matrix(word_dot_product)
+    min_dot = np.min(word_dot_product.T[polar_set_idx])
+    #print(f"min_dot: {min_dot}")
+    max_dot = np.max(word_dot_product.T[polar_set_idx])
+    #word_dp_softmax = softmax(word_dot_product.todense())
+    #print(f"max_dot: {max_dot}")
+    
+    dir_for_polar = os.path.join(dir_for_set, f"{polar_set_idx}_{negative_str}_{positive_str}.csv")
+
+    #print(f"word_distance:{word_distance.shape}")
+    #print(f"word_dot_product:{word_dot_product.T[polar_set_idx].shape}")
+
+    with open(dir_for_polar, 'w') as textfile:
+        textfile.write("Word, Value\n")
+        for idx in range(word_distance.shape[0]):
+            if CURRENT_CALC == CALCULATION[0]:
+                sign = 1.0
+                if word_dot_product.T[polar_set_idx].T[idx] < 0.0:
+                    sign = -1.0
+                textfile.write(f"{feature_list[idx]},{(GeneralNorm(word_distance[idx].data[0], min_dist, max_dist, 0.0, 1.0) * sign)}\n")
+            elif CURRENT_CALC == CALCULATION[1]:
+                value = word_dot_product.T[polar_set_idx].T[idx]
+                if len(value.data) == 0:
+                    value = 0.0
+                else:
+                    value = value.data[0]
+                norm_value = GeneralNorm(value, min_dot, max_dot, -1.0, 1.0)
+                #print(f"value: {value}, min_dot: {min_dot}, max_dot:{max_dot}, normalize: {norm_value}")
+                textfile.write(f"{feature_list[idx]},{norm_value}\n")
+           
+
+    del word_projected_point
+    del word_distance
+    del word_dot_product
